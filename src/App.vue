689d673e849b6cd692813913582d16e5fb13d467
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-03-03 21:44:31 +0900

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const message = ref('hello world')
-const cls = ref('msg')
+const count = ref(0)
+const message = ref('count!!!')
+const messageCssClass = ref('msg')
+const increment = () => {
+  count.value++
+}
+const reset = () => {
+  count.value = 0
+}
 </script>
 
 <template>
-  <h1 :class="cls">{{ message }}</h1>
+  <h1 :class="messageCssClass">{{ message }}</h1>
+  <button @click="increment">CLICK ME!!!</button>
+  <div>{{ count }}</div>
+  <button @click="reset">reset</button>
 </template>
 
 <style>
 .msg {
   color: red;
+  font-size: xx-large;
 }
 </style>
